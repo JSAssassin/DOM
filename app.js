e6df9dd32628a91ebba0817eebb55a54141abc82
Tashi D. Gyeltshen <tashidgyeltshen@hotmail.com>
2018-05-14 19:57:02 -0400

    added hide and show button for the list

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 //const myHeading = document.getElementById('myHeading'); OR
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
 const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const mytextInput = document.getElementById('myTextInput');
 
-myButton.addEventListener('click',()=>{
+toggleList.addEventListener('click', () => {
+    if (listDiv.style.display == 'none') {
+        toggleList.textContent = 'Hide List';
+        listDiv.style.display = 'block';
+    } else {
+        toggleList.textContent = 'Show List';
+        listDiv.style.display = 'none';
+    }
+});
+
+myButton.addEventListener('click', () => {
     myHeading.style.color = myTextInput.value;
 });
 
 const myList = document.getElementsByTagName('li');
 
-for(let i=0; i<myList.length; i++){
+for (let i = 0; i < myList.length; i++) {
     myList[i].style.color = 'purple';
 }
 
 // const errorNotPurple = document.getElementsByClassName('error-not-purple');OR
 const errorNotPurple = document.querySelectorAll('.error-not-purple');
 
-for(let i=0; i<errorNotPurple.length; i++){
+for (let i = 0; i < errorNotPurple.length; i++) {
     errorNotPurple[i].style.color = 'red';
 }
 
 const even = document.querySelectorAll('li:nth-child(odd)');
 
-for(let i=0; i<even.length; i++){
+for (let i = 0; i < even.length; i++) {
     even[i].style.backgroundColor = 'lightgrey';
 }
 
@@ -31,5 +43,8 @@ const p = document.querySelector('p.description');
 const button = document.querySelector('button.description');
 
 button.addEventListener('click', () => {
-    p.textContent = input.value + ':';
+    // p.textContent = input.value + ':'; OR
+    p.innerHTML = input.value + ':';
 });
+
+p.title = "List Description";
